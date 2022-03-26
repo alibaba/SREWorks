07d00ea012707ff91563e295a97270f1d9012767
sreworks <dataops@service.alibaba.com>
2022-03-26 21:25:15 +0800

    appmanager鉴权相关优化

--- paas/appmanager/APP-META-PRIVATE/postrun/02_init_trait_definition.py
+++ paas/appmanager/APP-META-PRIVATE/postrun/02_init_trait_definition.py
@@ -6,6 +6,8 @@ import os
 import sys
 
 import requests
+from oauthlib.oauth2 import LegacyApplicationClient
+from requests_oauthlib import OAuth2Session
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -18,14 +20,47 @@ logger.addHandler(handler)
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 ENDPOINT = 'http://' + os.getenv('ENDPOINT_PAAS_APPMANAGER')
+CLIENT_ID = os.getenv('APPMANAGER_CLIENT_ID')
+CLIENT_SECRET = os.getenv('APPMANAGER_CLIENT_SECRET')
+USERNAME = os.getenv('APPMANAGER_ACCESS_ID')
+PASSWORD = os.getenv('APPMANAGER_ACCESS_SECRET')
 
 
-def apply(post_body):
+class AppManagerClient(object):
+
+    def __init__(self, endpoint, client_id, client_secret, username, password):
+        os.environ.setdefault('OAUTHLIB_INSECURE_TRANSPORT', '1')
+        self._endpoint = endpoint
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._username = username
+        self._password = password
+        self._token = self._fetch_token()
+
+    @property
+    def client(self):
+        return OAuth2Session(self._client_id, token=self._token)
+
+    def _fetch_token(self):
+        """
+        获取 appmanager access token
+        """
+        oauth = OAuth2Session(client=LegacyApplicationClient(client_id=CLIENT_ID))
+        return oauth.fetch_token(
+            token_url=os.path.join(ENDPOINT, 'oauth/token'),
+            username=self._username,
+            password=self._password,
+            client_id=self._client_id,
+            client_secret=self._client_secret
+        )
+
+
+def apply(r, post_body):
     """
     导入 post_body 对应的 trait 数据
     :return:
     """
-    response = requests.post(ENDPOINT + '/traits', data=post_body)
+    response = r.post(ENDPOINT + '/traits', data=post_body)
     response_json = response.json()
     if response_json.get('code') != 200:
         logger.error('import trait to appmanager failed, response=%s' % response.text)
@@ -34,6 +69,12 @@ def apply(post_body):
 
 
 def apply_all_traits():
+    try:
+        r = AppManagerClient(ENDPOINT, CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD).client
+    except Exception as e:
+        logger.error("cannot find appmanager client auth info, skip")
+        r = requests
+
     # 读取所有配置
     config_map = {}
     path = os.path.join(CURRENT_PATH, 'traits')
@@ -45,7 +86,7 @@ def apply_all_traits():
 
     for name in config_map:
         post_body = config_map[name]
-        apply(post_body)
+        apply(r, post_body)
 
 
 if __name__ == '__main__':

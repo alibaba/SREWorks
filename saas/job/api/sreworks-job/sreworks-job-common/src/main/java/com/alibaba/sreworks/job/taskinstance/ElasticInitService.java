81718c5ef093bf3dd79759718fa611db417bd29a
sreworks <dataops@service.alibaba.com>
2022-04-29 13:26:36 +0800

    适配ES优化

--- saas/job/api/sreworks-job/sreworks-job-common/src/main/java/com/alibaba/sreworks/job/taskinstance/ElasticInitService.java
+++ saas/job/api/sreworks-job/sreworks-job-common/src/main/java/com/alibaba/sreworks/job/taskinstance/ElasticInitService.java
@@ -1,43 +1,29 @@
 package com.alibaba.sreworks.job.taskinstance;
 
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 public class ElasticInitService extends AbstractElasticsearchConfiguration {
     @Autowired
     ElasticsearchRestClientProperties elasticsearchRestClientProperties;
-    //
-    //@Bean
-    //public RestHighLevelClient restHighLevelClient(@Autowired RestClientBuilder restClientBuilder) {
-    //    return new RestHighLevelClient(
-    //        restClientBuilder
-    //            .setHttpClientConfigCallback(requestConfig -> requestConfig
-    //                .setKeepAliveStrategy((response, context) -> TimeUnit.MINUTES.toMillis(3))
-    //            )
-    //    );
-    //
-    //}
 
     @Override
     @Bean(destroyMethod = "close")
     public RestHighLevelClient elasticsearchClient() {
-        log.info("elasticsearchRestClientProperties.getUris(): {}", elasticsearchRestClientProperties.getUris());
+        String uri = elasticsearchRestClientProperties.getUris().get(0);
+        uri = uri.replace("http://", "").replace("https://", "");
+        log.info("elasticsearchRestClientProperties.getUris(): {}", uri);
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-            .connectedTo(elasticsearchRestClientProperties.getUris().get(0))
+            .connectedTo(uri)
             .withBasicAuth(
                 elasticsearchRestClientProperties.getUsername(),
                 elasticsearchRestClientProperties.getPassword()

756dd4a2873c9c2cd8f53696af4b0cda4a6c0d1f
fangzong.lyj <fangzong.lyj@alibaba-inc.com>
2022-05-31 15:03:43 +0800

    feat: support detail frontend search

--- saas/search/api/tkg-one/tkg-one-server/src/main/java/com/alibaba/tesla/tkgone/server/common/Constant.java
+++ saas/search/api/tkg-one/tkg-one-server/src/main/java/com/alibaba/tesla/tkgone/server/common/Constant.java
@@ -210,4 +210,12 @@ public final class Constant {
     public final static String BACKEND_STORE_PATTERNS_SPLIT_SYMBOL = ";";
     public final static int HASHMAP_INIT_CAPACITY = 1000;
 
+    /*** content 类型 ***/
+    public final static String FRONTEND_CONTENT = "frontend";
+
+    /*** var 类型 ***/
+    public enum FRONTEND_VAR_TYPE {
+        api, datasource, constant
+    }
+
 }

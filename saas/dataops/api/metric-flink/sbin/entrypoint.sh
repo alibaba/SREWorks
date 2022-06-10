ea73d60bf86c94e1d12199b96199b847d5c8de46
twwy <jiongen.zje@alibaba-inc.com>
2022-06-10 13:18:36 +0800

    chore: metric-flink code optimize

--- saas/dataops/api/metric-flink/sbin/entrypoint.sh
+++ saas/dataops/api/metric-flink/sbin/entrypoint.sh
@@ -3,6 +3,27 @@
 set -x
 set -e
 
+export DATA_DB_HOST=${DATA_DB_HOST}
+export DATA_DB_PORT=${DATA_DB_PORT}
+export DATA_DB_HEALTH_NAME=${DATA_DB_HEALTH_NAME}
+export DATA_DB_USER=${DATA_DB_USER}
+export DATA_DB_PASSWORD=${DATA_DB_PASSWORD}
+
+export HEALTH_ENDPOINT=${HEALTH_ENDPOINT}
+
+export MINIO_ENDPOINT=${MINIO_ENDPOINT}
+export MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY}
+export MINIO_SECRET_KEY=${MINIO_SECRET_KEY}
+
+export UDF_ARTIFACT_NAME="metric-flink-12"
+export UDF_ARTIFACT_JAR="metric-flink-1.2.jar"
+
+export VVP_ENDPOINT=${VVP_ENDPOINT}
+export KAFKA_URL=${KAFKA_URL}
+export ES_URL=${ES_URL}
+
+export VVP_WORK_NS="default"
+
 /bin/sh /app/sbin/build.sh
 
 /bin/sh /app/sbin/upload.sh
--- saas/dataops/api/metric-flink/sbin/upload.sh
+++ saas/dataops/api/metric-flink/sbin/upload.sh
@@ -2,10 +2,6 @@
 
 SW_ROOT=$(cd `dirname $0`; pwd)
 
-export MINIO_ENDPOINT=${MINIO_ENDPOINT}
-export MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY}
-export MINIO_SECRET_KEY=${MINIO_SECRET_KEY}
-
 $SW_ROOT/mc alias set sw http://${MINIO_ENDPOINT} ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 
 ###### UPLOAD BUILD-IN RULES CONF
@@ -14,5 +10,5 @@ $SW_ROOT/mc cp /app/sbin/vvp-resources/rules.json sw/metric-rules/sreworks/metri
 
 ###### UPLOAD MINIO UDF ARTIFACT JAR
 $SW_ROOT/mc mb -p sw/vvp
-$SW_ROOT/mc cp /app/sbin/metric-alarm-1.2-SNAPSHOT.jar sw/vvp/artifacts/namespaces/default/udfs/
+$SW_ROOT/mc cp /app/sbin/${UDF_ARTIFACT_JAR} sw/vvp/artifacts/namespaces/default/udfs/
 

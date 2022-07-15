1478cc703dad293c65146f567f5246585865fba4
fangzong.lyj <fangzong.lyj@alibaba-inc.com>
2022-07-15 16:07:52 +0800

    fix:warehouse doc count bugfix

--- saas/dataops/api/warehouse/warehouse-service/src/main/java/com/alibaba/sreworks/warehouse/operator/ESSearchOperator.java
+++ saas/dataops/api/warehouse/warehouse-service/src/main/java/com/alibaba/sreworks/warehouse/operator/ESSearchOperator.java
@@ -2,14 +2,20 @@ package com.alibaba.sreworks.warehouse.operator;
 
 import com.alibaba.sreworks.warehouse.common.client.ESClient;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.client.GetAliasesResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.core.CountRequest;
 import org.elasticsearch.client.core.CountResponse;
+import org.elasticsearch.cluster.metadata.AliasMetadata;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Set;
 
 
 /**
@@ -24,8 +30,8 @@ public class ESSearchOperator {
     @Autowired
     ESClient esClient;
 
-    public long countDoc(String index) throws Exception {
-        CountRequest countRequest = new CountRequest(index);
+    public long countDocByIndices(Set<String> indices) throws Exception {
+        CountRequest countRequest = new CountRequest(indices.toArray(new String[0]));
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         searchSourceBuilder.query(QueryBuilders.matchAllQuery());
         countRequest.source(searchSourceBuilder);
@@ -34,5 +40,15 @@ public class ESSearchOperator {
         CountResponse countResponse = hlClient.count(countRequest, RequestOptions.DEFAULT);
 
         return countResponse.getCount();
+    }
+
+    public long countDocByAlias(String alias) throws Exception {
+        GetAliasesRequest requestWithAlias = new GetAliasesRequest(alias);
+
+        RestHighLevelClient hlClient = esClient.getHighLevelClient();
+        GetAliasesResponse aliasesResponse = hlClient.indices().getAlias(requestWithAlias, RequestOptions.DEFAULT);
+        Map<String, Set<AliasMetadata>> aliases = aliasesResponse.getAliases();
+        Set<String> indices = aliases.keySet();
+        return countDocByIndices(indices);
     }
 }

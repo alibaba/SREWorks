e8014f113a292160dc6fb22379ba541e69324c5d
wangwei <deeham.ww@alibaba-inc.com>
2022-07-21 17:06:43 +0800

    fix: build config test

--- paas/sw-frontend/config/webpack.config.prod.js
+++ paas/sw-frontend/config/webpack.config.prod.js
@@ -280,9 +280,6 @@ module.exports = {
             staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
         }),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new webpack.DefinePlugin({
-            THEMES: JSON.stringify(ThemeVariables)
-        })
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.

9d139b0865d8035322259a9eac9543fb4a8bcd00
doraemonext <yaoxing.gyx@alibaba-inc.com>
2022-07-25 17:05:00 +0800

    feat: add permission check when getting app list

--- paas/appmanager/tesla-appmanager-server/src/main/java/com/alibaba/tesla/appmanager/server/controller/MarketController.java
+++ paas/appmanager/tesla-appmanager-server/src/main/java/com/alibaba/tesla/appmanager/server/controller/MarketController.java
@@ -268,7 +268,7 @@ public class MarketController extends AppManagerBaseController {
         if(request.getAppOptions() != null) {
             appMetaUpdateReq.setOptions(request.getAppOptions());
         }
-        appMetaProvider.save(appMetaUpdateReq);
+        appMetaProvider.save(appMetaUpdateReq, getOperator(auth));
 
         return buildSucceedResult(appPackageInfo);
     }

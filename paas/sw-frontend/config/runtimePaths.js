8baec079408af8bb88ea71e34daf0f564ef543f9
wangwei <deeham.ww@alibaba-inc.com>
2022-07-26 17:41:03 +0800

    fix: build config outputPath fix

--- paas/sw-frontend/config/runtimePaths.js
+++ paas/sw-frontend/config/runtimePaths.js
@@ -25,29 +25,30 @@ var reactPath = getDependencyVesion(packagejson, 'react');
 var react_dom_path = getDependencyVesion(packagejson, 'react-dom');
 var systemjsPath = getDependencyVesion(packagejson, 'systemjs');
 var bizchartsPath = getDependencyVesion(packagejson, 'bizcharts');
+
 const dependency_arr = [{
     from: paths.appNodeModules + '/antd/dist/antd.min.js',
-    to: paths.appPublic + '/common_vendor/antd/' + antdPath + '/antd.min.js'
+    to: paths.appBuild + '/common_vendor/antd/' + antdPath + '/antd.min.js'
 },
 {
     from: paths.appNodeModules + '/react/umd/react.production.min.js',
-    to: paths.appPublic + '/common_vendor/react/' + reactPath + '/react.production.min.js'
+    to: paths.appBuild + '/common_vendor/react/' + reactPath + '/react.production.min.js'
 },
 {
     from: paths.appNodeModules + '/react-dom/umd/react-dom.production.min.js',
-    to: paths.appPublic + '/common_vendor/react-dom/' + react_dom_path + '/react-dom.production.min.js'
+    to: paths.appBuild + '/common_vendor/react-dom/' + react_dom_path + '/react-dom.production.min.js'
 },
 {
     from: paths.appNodeModules + '/moment/min/moment.min.js',
-    to: paths.appPublic + '/common_vendor/moment/' + momentPath + '/moment.min.js'
+    to: paths.appBuild + '/common_vendor/moment/' + momentPath + '/moment.min.js'
 },
 {
     from: paths.appNodeModules + '/systemjs/dist/system.min.js',
-    to: paths.appPublic + '/common_vendor/systemjs/' + systemjsPath + '/system.min.js'
+    to: paths.appBuild + '/common_vendor/systemjs/' + systemjsPath + '/system.min.js'
 },
 {
     from: paths.appNodeModules + '/bizcharts/umd/BizCharts.min.js',
-    to: paths.appPublic + '/common_vendor/bizcharts/' + bizchartsPath + '/BizCharts.min.js'
+    to: paths.appBuild + '/common_vendor/bizcharts/' + bizchartsPath + '/BizCharts.min.js'
 }]
 console.log("init runtime paths successfull")
 

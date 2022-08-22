f9712b531a0470a7f55938a682597687fcb12cd4
fangzong.lyj <fangzong.lyj@alibaba-inc.com>
2022-08-22 17:33:09 +0800

    fix:init es with auth

--- saas/job/api/sreworks-job/APP-META-PRIVATE/postrun/00_init_es.py
+++ saas/job/api/sreworks-job/APP-META-PRIVATE/postrun/00_init_es.py
@@ -1,11 +1,16 @@
 import json
 import os
 import requests
+from requests.auth import HTTPBasicAuth
 
 #url = "http://prod-dataops-elasticsearch-master.sreworks-dataops:9200"
 url = os.environ.get("ES_ENDPOINT")
+username = os.environ.get("ES_USERNAME")
+password = os.environ.get("ES_PASSWORD")
 
 headers = {"Content-Type": "application/json"}
+
+basic = HTTPBasicAuth(username, password)
 
 data = {
     "persistent": {
@@ -13,7 +18,7 @@ data = {
     }
 }
 
-r = requests.put(url + "/_cluster/settings", data=json.dumps(data), headers=headers)
+r = requests.put(url + "/_cluster/settings", data=json.dumps(data), headers=headers, auth=basic)
 
 if r.status_code == 200:
     print(r.json())

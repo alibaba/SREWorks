8c78f520b7e03874fc72bb07cfb34d1a7ce9073f
see <upcwangying@126.com>
2022-08-31 10:10:07 +0800

    build: @sreworks/components

--- paas/frontend/packages/components/config/utils.js
+++ paas/frontend/packages/components/config/utils.js
@@ -2,12 +2,7 @@ const glob = require('glob')
 
 function getEntries() {
   let map = {}
-  const mainEntryFiles = glob.sync('./src/index.js?(x)')
-  mainEntryFiles.forEach((filepath) => {
-    let fileDir = /.\/src\/(.*?)\.jsx?/.exec(filepath)
-    map[fileDir[1]] = filepath
-  })
-  const entryFiles = glob.sync('./src/!(style)/index.js?(x)')
+  const entryFiles = glob.sync('./src{,/!(style)}/index.js?(x)')
   entryFiles.forEach((filepath) => {
     let fileDir = /.\/src\/(.*?)\.jsx?/.exec(filepath)
     map[fileDir[1]] = filepath
--- paas/frontend/packages/components/gulpfile.js
+++ paas/frontend/packages/components/gulpfile.js
@@ -2,13 +2,12 @@ const less = require('gulp-less')
 const postcss = require('gulp-postcss')
 const cleanCSS = require('gulp-clean-css')
 const path = require('path')
-const rimraf = require('rimraf')
 const merge2 = require('merge2')
+const through2 = require('through2')
 const gulp = require('gulp')
 
 function compile(module) {
   const dir = module ? './es' : './lib'
-  // rimraf.sync(dir)
   const lessToCss = gulp
     .src('./src/**/*.less')
     .pipe(
@@ -20,8 +19,32 @@ function compile(module) {
     .pipe(cleanCSS())
     .pipe(gulp.dest(dir))
 
+  const toJs = gulp
+    .src('./src/**/style/index.js?(x)')
+    .pipe(
+      through2.obj(function (file, encoding, next) {
+        const cloneFile = file.clone()
+        if (cloneFile.path.endsWith('jsx')) {
+          cloneFile.path = cloneFile.path.replace(/index\.jsx/, 'index.js')
+        }
+        const cloneCssFile = cloneFile.clone()
+        this.push(cloneFile)
+        if (cloneCssFile.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+          const content = cloneCssFile.contents.toString(encoding)
+          cloneCssFile.contents = Buffer.from(content.replace(/\.less/g, '.css'))
+          cloneCssFile.path = cloneCssFile.path.replace(/index\.js/, 'css.js')
+          this.push(cloneCssFile)
+          next()
+        } else {
+          next()
+        }
+      }),
+    )
+    .pipe(gulp.dest(dir))
+
   const lessToLib = gulp.src('./src/**/*.less').pipe(gulp.dest(dir))
-  return [lessToCss, lessToLib]
+  const assets = gulp.src(['./src/**/*.@(png|svg)']).pipe(gulp.dest(dir))
+  return [lessToCss, lessToLib, toJs, assets]
 }
 
 gulp.task('default', function () {

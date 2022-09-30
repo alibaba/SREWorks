ab60867d19f51ea636e17432cd700fef73354ab5
twwy <jiongen.zje@alibaba-inc.com>
2022-09-30 15:34:13 +0800

    fix: groupId com.alibaba.sreworks -> io.sreworks

---
+++ saas/app/api/app/appdev-server/src/main/java/io/sreworks/appdev/server/services/AppmanagerService.java
@@ -0,0 +1,40 @@
+package io.sreworks.appdev.server.services;
+
+import com.alibaba.sreworks.common.util.AppmanagerServiceUtil;
+import com.alibaba.sreworks.common.util.JsonUtil;
+import io.sreworks.common.util.Requests;
+
+import com.alibaba.sreworks.domain.DO.App;
+import com.alibaba.tesla.web.constant.HttpHeaderNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import static com.alibaba.sreworks.domain.utils.AppUtil.appmanagerId;
+
+@Slf4j
+@Service
+public class AppmanagerService {
+
+    public void create(App app) throws Exception {
+        log.info("POST " + AppmanagerServiceUtil.getEndpoint() + "/apps");
+        new Requests(AppmanagerServiceUtil.getEndpoint() + "/apps")
+                .postJson(
+                        "appId", app.getName(),
+                        "options", JsonUtil.map(
+                                "name", app.getName(),
+                                "source", "app",
+                                "apiVersion", "v2"
+                        )
+                )
+                .headers(HttpHeaderNames.X_EMPL_ID, app.getCreator())
+                .post().isSuccessful();
+    }
+
+    public void delete(String appId, String user) throws Exception {
+        log.info("DELETE " + AppmanagerServiceUtil.getEndpoint() + "/apps/" + appId);git
+        new Requests(AppmanagerServiceUtil.getEndpoint() + "/apps/" + appId)
+                .headers(HttpHeaderNames.X_EMPL_ID, user)
+                .delete().isSuccessful();
+    }
+
+}

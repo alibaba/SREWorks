3808f42f1718173c8c653b5f7371d741d3e96ee2
wangwei <deeham.ww@alibaba-inc.com>
2022-10-10 16:27:24 +0800

    feat: @sreworks/components

--- paas/frontend/packages/components/src/index.js
+++ paas/frontend/packages/components/src/index.js
@@ -1,1 +1,57 @@
-export { Loading } from './loading'
+import Loading from './loading'
+import AceViewer from './AceViewer'
+import CardWrapper from './CardWrapper'
+import DateRange from './DateRange'
+import DraggableTabs from './DraggableTabs'
+import Dynamic from './Dynamic'
+import EditableCut from './EditableCut'
+import EditableTable from './EditableTable'
+import EllipsisTip from './EllipsisTip'
+import ErrorBoundary from './ErrorBoundary'
+import FormElementFactory from './FormBuilder/FormElementFactory'
+import FormElementType from './FormBuilder/FormElementType'
+import SimpleForm from './FormBuilder/SimpleForm'
+import FullScreenTool from './FullScreenTool'
+import GridCheckBox from './GridCheckBox'
+import JsonEditor from './JsonEditor'
+import JSXRender from './JSXRender'
+import Login from './Login'
+import NotFound from './NotFound'
+import NoticeBoardBar from './NoticeBoardBar'
+import NotifyCenter from './NotifyCenter'
+import PagingTable from './PagingTable'
+import PopoverConfirm from './PopoverConfirm'
+import RenderFactory from './RenderFactory'
+import SelectedTable from './SelectedTable'
+import SRECron from './SRECron'
+import TIcon from './TIcon'
+
+export {
+  Loading,
+  AceViewer,
+  CardWrapper,
+  DateRange,
+  DraggableTabs,
+  Dynamic,
+  EditableCut,
+  EditableTable,
+  EllipsisTip,
+  ErrorBoundary,
+  FormElementFactory,
+  FormElementType,
+  SimpleForm,
+  FullScreenTool,
+  GridCheckBox,
+  JsonEditor,
+  JSXRender,
+  Login,
+  NotFound,
+  NoticeBoardBar,
+  NotifyCenter,
+  PagingTable,
+  PopoverConfirm,
+  RenderFactory,
+  SelectedTable,
+  SRECron,
+  TIcon,
+}

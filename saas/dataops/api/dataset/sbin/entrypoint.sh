59e02b69b7cdb2ea4b86357b771a475b017fe2df
fangzong.lyj <fangzong.lyj@alibaba-inc.com>
2022-10-24 14:55:04 +0800

    feat:dataops init

--- saas/dataops/api/dataset/sbin/entrypoint.sh
+++ saas/dataops/api/dataset/sbin/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-nohup python /app/postrun/00_init_job.py >postrun.log 2>&1 &
+nohup python3 /app/postrun/00_init_job.py >postrun.log 2>&1 &
 
 /app/sbin/run.sh
--- saas/health/api/health/APP-META-PRIVATE/db/2_alter_failure_record_index.up.sql
+++ saas/health/api/health/APP-META-PRIVATE/db/2_alter_failure_record_index.up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `failure_record` DROP FOREIGN KEY def_ibfk_7;
 ALTER TABLE `failure_record` DROP FOREIGN KEY incident_ibfk_2;
+ALTER TABLE `incident_type` ADD UNIQUE `key_label`(label);

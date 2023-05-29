feef841f868552a6ee2e2ee2b66013e9867af9e6
twwy <jiongen.zje@alibaba-inc.com>
2023-05-29 18:15:32 +0800

    fix: app baseline update

--- saas/job/api/sreworks-job/APP-META-PRIVATE/db/3_stream-job-index.up.sql
+++ saas/job/api/sreworks-job/APP-META-PRIVATE/db/3_stream-job-index.up.sql
@@ -1,11 +1,3 @@
 ALTER TABLE `sreworks_stream_job`
     ADD Unique KEY `name`(`name`)
 ;
-
-ALTER TABLE `sreworks_stream_job_block`
-    ADD UNIQUE KEY `stream_job_index` (`stream_job_id`,`name`,`block_type`)
-;
-
-ALTER TABLE `sreworks_stream_job_block`
-    ADD KEY `stream_job_id` (`stream_job_id`)
-;

ebdcca36e0f155d569a1884e93c0c9ca6d0afe8b
csb <shuaibiao.csb@alibaba-inc.com>
2023-06-05 16:12:46 +0800

    Update Property.js

--- paas/frontend/packages/shared-utils/src/Property.js
+++ paas/frontend/packages/shared-utils/src/Property.js
@@ -2,13 +2,7 @@ class Property {
   constructor(props) {
     this.properties = {
       ENV: {
-        //运行环境定义
-        ApsaraStack: 'ApsaraStack', //标专
-        DXZ: 'DXZ', //大小专
-        OXS: 'OXS', //OXS
-        Internal: 'Internal', //对内()
         Standalone: 'Standalone', //软件化输出
-        RQY: 'RQY', //敏捷版
         PaaS: 'PaaS', //paas化版本
       },
       name: 'SREWorks',

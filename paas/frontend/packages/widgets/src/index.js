8735e05ad10a45f104154fc372ada0fc58d98a1d
guishun80808 <guishun2014@163.com>
2023-08-16 16:59:22 +0800

    Update index.js
    win环境组件加载失败问题

--- paas/frontend/packages/widgets/src/index.js
+++ paas/frontend/packages/widgets/src/index.js
@@ -2,21 +2,27 @@ import Property from './Property'
 let SRE_builtInWidgets = {}
 let SRE_builtInWidgetMetaMapping = {}
 const SRE_widgetsContext = require.context(
-  './',
+  './srewidgets/',
   true,
-  /^\.\/srewidgets\/((?!\/)[\s\S])+\/index\.js$/,
+  // /^\.\/srewidgets\/((?!\/)[\s\S])+\/index\.js$/,
+  /index\.js$/,
 )
 SRE_widgetsContext.keys().forEach((key) => {
-  let widgetName = key.split('/')[2]
+  // let widgetName = key.split('/')[2]
+  let reg = new RegExp('index.js', 'g')
+  let widgetName = key.split('/')[1].replace(reg, '')
   SRE_builtInWidgets[widgetName] = SRE_widgetsContext(key)
 })
 const SRE_widgetMetasContext = require.context(
-  './',
+  './srewidgets/',
   true,
-  /^\.\/srewidgets\/((?!\/)[\s\S])+\/meta\.js$/,
+  // /^\.\/srewidgets\/((?!\/)[\s\S])+\/meta\.js$/,
+  /meta\.js$/,
 )
 SRE_widgetMetasContext.keys().forEach((key) => {
-  let widgetName = key.split('/')[2]
+  // let widgetName = key.split('/')[2]
+  let reg = new RegExp('index.js', 'g')
+  let widgetName = key.split('/')[1].replace(reg, '')
   const meta = SRE_widgetMetasContext(key)
   if (!meta.type) {
     meta.type = widgetName
